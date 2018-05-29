0d4d512fe9eebfe98ddc733b4f5ac9d80856a33c
Belén Garcia <belengarcia@MacBook-Pro-de-pc1177.local>
2018-05-29 23:17:31 +0200

    mandos right, left y top (sin gravedad) y animación

--- src/game.js
+++ src/game.js
@@ -17,20 +17,28 @@ Llamar a las funciones de Move
 function Game (canvasElement){
     this.ctx = canvasElement.getContext("2d");
     this.bk = new Background(this.ctx);
+    this.mario = new Mario(this.ctx);
+
+    this.setKeyboardListeners();
 }
 
 Game.prototype.start = function(){
     this.gameLoop = setInterval(function (){
         this.clearAll();
         this.drawAll();
+        this.moveAll();
     }.bind(this), 16)
 }
 
 
 Game.prototype.drawAll = function (){
     this.bk.draw();
+    this.mario.draw();
 }
 
+Game.prototype.moveAll = function (){
+    this.mario.move();
+}
 
 Game.prototype.clearAll = function (){
     this.ctx.clearRect(
@@ -39,4 +47,15 @@ Game.prototype.clearAll = function (){
         this.ctx.canvas.width,
         this.ctx.canvas.height
     );
+}
+
+Game.prototype.setKeyboardListeners = function (){
+    document.onkeydown = function(event) {
+        this.mario.onKeyDown(event.keyCode);
+      }.bind(this);
+    
+      document.onkeyup = function(event) {
+        this.mario.onKeyUp(event.keyCode);
+      }.bind(this);
+// esta la he copiado tal cual y no la entiendo!
 }
--- src/mario.js
+++ src/mario.js
@@ -22,3 +22,96 @@ bullets
 *Para más adelante: pensar en otras posibilidades de Mario
 
 */
+
+function Mario(ctx) {
+    this.ctx = ctx;
+    this.x = ctx.canvas.width/20;
+    this.width = 100;
+    this.height = this.width*3/4;
+    this.y = ctx.canvas.height - this.height - 60;
+
+    this.vx = 0;
+    this.vy = 0;
+
+    this.g = 0;
+    //en un futuro: this.vy, this.v, this.g
+
+    this.img = new Image();
+    this.img.src = 'img/mario.png'
+
+    this.img.frames = 3;
+    this.img.frameIndex = 0;
+    this.img.animateEvery = 10;
+
+    this.countFrames = 0;
+    }
+
+Mario.prototype.draw = function() {
+    this.ctx.drawImage(
+        this.img,
+        this.img.frameIndex * this.img.width / this.img.frames, 
+        0,
+        this.img.width/this.img.frames,
+        this.img.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+    )
+
+    this.countFrames++;
+}
+
+Mario.prototype.animate = function (){
+
+    this.img.frameIndex++;
+  
+    if (this.img.frameIndex >= this.img.frames) {
+        this.img.frameIndex = 0;
+    }
+
+}
+
+Mario.prototype.move = function (){
+
+    if (this.countFrames % this.img.animateEvery === 0) {
+        this.animate();
+        this.countFrames = 0;
+      } 
+
+    this.x += this.vx;
+    this.y += this.vy;
+}
+
+Mario.prototype.isJumping = function () {
+    
+}
+
+Mario.prototype.RIGHT = 39;
+Mario.prototype.LEFT = 37;
+Mario.prototype.TOP = 38;
+
+Mario.prototype.onKeyDown = function (code){
+    switch (code){
+        case this.RIGHT:
+            this.vx = 10;
+            break;
+        case this.LEFT:
+            this.vx = -10;
+            break;
+        case this.TOP:
+            this.vy = -5;
+            break;
+    }
+}
+
+Mario.prototype.onKeyUp = function (code){
+    switch (code){
+        case this.RIGHT:
+        case this.LEFT:
+            this.vx = 0;
+            break;
+        case this.TOP:
+            this.vy = 0;
+    }
+}
